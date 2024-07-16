d1d4367428e092c4bab25e707211496e38ae0138
yammei <gmeil.antin@gmail.com>
2024-07-15 23:02:14 -0700

    added dense layer + fixed pool layer sliding

--- README.md
+++ README.md
@@ -1,7 +1,66 @@
 <h1>Convolutional Neural Network Framework Project</h1>
 
 <h1>Progress</h1>
-<img src='https://github.com/yammei/convolution/blob/main/images/progress.png'/>
+
+```bash
+
+■ 1 ■                                                                           
+
+
+METHOD     generate_RGB_matrix(img_path: str)
+VARIABLE   width, height = (32, 32)
+RETURN     Status: Success | Type: numpy.ndarray | Size: 3.216KB
+
+
+■ 2 ■                                                                           
+
+
+METHOD     generate_kernels(kernel_weight: float, kernel_size: int, kernel_num: int)
+VARIABLE   kernels.shape = (3, 3, 3, 16)
+RETURN     Status: Success | Type: numpy.ndarray | Size: 3.616KB
+
+
+■ 3 ■                                                                           
+
+
+METHOD     convolve(rgb_matrix: numpy.ndarray, kernels: numpy.ndarray, kernel: KernelConfig)
+VARIABLE   feature_map.shape = (30, 30, 16)
+RETURN     Status: Success | Type: numpy.ndarray | Size: 115.344KB
+
+
+■ 4 ■                                                                           
+
+
+METHOD     pool(feature_map: numpy.ndarray, pool_size: int, pool_mode: str)
+VARIABLE   pooled_map.shape = (15, 15, 16)
+RETURN     Status: Success | Type: numpy.ndarray | Size: 28.944KB
+
+
+■ 5 ■                                                                           
+
+
+METHOD     flat(pooled_map: numpy.ndarray)
+VARIABLE   flattened_map.shape = (3600,)
+RETURN     Status: Success | Type: numpy.ndarray | Size: 28.912KB
+
+
+■ 6 ■                                                                           
+
+
+METHOD     dense(flattened_map: numpy.ndarray, neurons: int)
+VARIABLE   flattend_map.shape = (3600,) | weights.shape = (64, 3600) | weighted_map.shape = (64,)
+RETURN     Status: Success | Type: numpy.ndarray | Size: 0.624KB
+
+
+■ Computation Details ■
+
+
+CUM SUM    feature_map:   2763.535
+CUM SUM    pooled_map:    2223.125
+CUM SUM    flattened_map: 2223.125
+CUM SUM    weighted_map:  71171.295
+
+```
 
 <h1>Dependencies</h1>
 
--- framework/cnn_computation.py
+++ framework/cnn_computation.py
@@ -71,8 +71,8 @@ def pool(feature_map: np.ndarray, pool_size: int = 2, pool_stride: int = 2, pool
     pooled_map: np.ndarray = np.zeros((output_height, output_width, kernel.num))
 
     for k in range(kernel.num):
-        for i in range(input_height - pool_size + 1, pool_stride):
-            for j in range(input_width - pool_size + 1, pool_stride):
+        for i in range(0, input_height - pool_size + 1, pool_stride):
+            for j in range(0, input_width - pool_size + 1, pool_stride):
                 # Downsamples feature map by taking max value in a (pool_size x pool_size) sliding window for all feature maps (k).
                 pooled_map[i//pool_stride, j//pool_stride] = np.max(feature_map[i:i+pool_size, j:j+pool_size, k])
 
@@ -80,15 +80,55 @@ def pool(feature_map: np.ndarray, pool_size: int = 2, pool_stride: int = 2, pool
     ML.end(1, pooled_map)
     return pooled_map
 
+# Flattens multi-dimensional array to 1D array to translate to full connective (dense) layers; providing the number of input features.
+# (e.g., for a 3D array of size (15, 15, 16) you have 15 * 15 * 16 or 3600 input features)
+def flat(pooled_map: np.ndarray) -> np.ndarray:
+    ML.start('flat', {'pooled_map': type(pooled_map)})
+    flattened_map = pooled_map.flatten()
+    log(f"VARIABLE   flattened_map.shape = {flattened_map.shape}")
+    ML.end(1, flattened_map)
+    return flattened_map
+
+# K. Sandhya. (2021). How do determine the number of layers and neurons in the hidden layer?. Geek Culture. https://medium.com/geekculture/introduction-to-neural-network-2f8b8221fbd3
+def dense(flattened_map: np.ndarray, neurons: int = 64) -> np.ndarray:
+    ML.start('dense', {'flattened_map': type(flattened_map), 'neurons': type(neurons)})
+
+    features: int = flattened_map.shape[0]
+    weights: np.ndarray = np.random.rand(neurons, features)
+    biases: np.ndarray = np.random.rand(neurons)
+
+    # Dot product operation. Creates n-amount of neurons with weighed then biased input features.
+    # SUM from j=0 to n=neurons of WEIGHT_ij * INPUT_i
+    weighted_map = np.dot(weights, flattened_map) + biases
+
+    log(f"VARIABLE   flattend_map.shape = {flattened_map.shape} | weights.shape = {weights.shape} | weighted_map.shape = {weighted_map.shape}")
+
+    ML.end(1, weighted_map)
+    return weighted_map
+
 '''
+# Matrix Multiplication Test Script
 np.random.seed(1)
 rand_matrix_1 = np.random.randint(50, 201, size=(6, 7))
 rand_matrix_2 = np.random.randint(50, 201, size=(7, 9))
 multiply_matrices(matrix_1=rand_matrix_1, matrix_2=rand_matrix_2)
 '''
 
+np.random.seed(1)
 img_path: str = '../images/cat.png'
 test_rgb_matrix: np.ndarray = generate_RGB_matrix(img_path)
 default_kernels: np.ndarray = generate_kernels()
 feature_map = convolve(test_rgb_matrix, default_kernels)
 pooled_map = pool(feature_map)
+flattened_map = flat(pooled_map)
+weighted_map = dense(flattened_map)
+
+log(f"\n■ Computation Details ■\n\n")
+log(f"CUM SUM    feature_map:   {sum(sum(sum(feature_map))):.3f}")
+log(f"CUM SUM    pooled_map:    {sum(sum(sum(pooled_map))):.3f}")
+log(f"CUM SUM    flattened_map: {sum(flattened_map):.3f}")
+log(f"CUM SUM    weighted_map:  {sum(weighted_map):.3f}")
+log(f"\n")
+
+# Resources
+# L. Yongxin et al. (2023). Zero-Bias Deep Learning for Accurate Identification of Internet-of-Things (IoT) Devices. IEEE Internet of Things Journal. https://ieeexplore.ieee.org/document/9173537
